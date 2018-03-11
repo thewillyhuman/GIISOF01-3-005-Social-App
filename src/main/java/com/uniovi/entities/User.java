1bb16192f9c9b9c0b1c142bc0f426a10a6ee5e09
Guillermo Facundo Colunga <colunga91@gmail.com>
2018-03-11 20:06:53 +0100

    Improved the entities and the tests.
    - Added the category of the tests for the integration, smooke, unit tests.
    - Added JPA tags to the entities.
    
    Signed-off-by: Guillermo Facundo Colunga <colunga91@gmail.com>

--- src/main/java/com/uniovi/entities/User.java
+++ src/main/java/com/uniovi/entities/User.java
@@ -1,93 +1,120 @@
 package com.uniovi.entities;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USER")
 public class User {
-    
+
+    @Id
+    @GeneratedValue
+    private long id;
+
     private String name;
+
+    @Column(unique = true)
     private String email;
+
     private String password;
+
+    @Transient
     private String passwordConfirm;
-    
+
     // List of friends.
-    private List<User> friends = new ArrayList<User>();
-    
+    @ManyToMany(cascade = { CascadeType.ALL })
+    @JoinTable(name = "friends", joinColumns = @JoinColumn(name = "friend_id"), inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private Set<User> friends = new HashSet<User>();
+
     // List of petitions.
-    private List<FriendRequest> firendRequests = new ArrayList<FriendRequest>();
-    
+    private Set<FriendRequest> friendRequests = new HashSet<FriendRequest>();
+
     public User(String name, String email) {
 	setName(name);
 	setEmail(email);
 	setPassword(password);
     }
-    
+
     public User() {
-	
+
     }
 
     /**
      * @return the name
      */
     public String getName() {
-        return name;
+	return name;
     }
 
     /**
-     * @param name the name to set
+     * @param name
+     *            the name to set
      */
     public void setName(String name) {
-        this.name = name;
+	this.name = name;
     }
 
     /**
      * @return the email
      */
     public String getEmail() {
-        return email;
+	return email;
     }
 
     /**
-     * @param email the email to set
+     * @param email
+     *            the email to set
      */
     public void setEmail(String email) {
-        this.email = email;
+	this.email = email;
     }
 
     /**
      * @return the password
      */
     public String getPassword() {
-        return password;
+	return password;
     }
 
     /**
-     * @param password the password to set
+     * @param password
+     *            the password to set
      */
     public void setPassword(String password) {
-        this.password = password;
+	this.password = password;
     }
 
     /**
      * @return the passwordConfirm
      */
     public String getPasswordConfirm() {
-        return passwordConfirm;
+	return passwordConfirm;
     }
 
     /**
-     * @param passwordConfirm the passwordConfirm to set
+     * @param passwordConfirm
+     *            the passwordConfirm to set
      */
     public void setPasswordConfirm(String passwordConfirm) {
-        this.passwordConfirm = passwordConfirm;
+	this.passwordConfirm = passwordConfirm;
     }
 
-    public List<User> getFriends() {
+    public Set<User> getFriends() {
 	return friends;
     }
 
-    public void setFriends(List<User> friends) {
+    public void setFriends(Set<User> friends) {
 	this.friends = friends;
+    }
+
+    public Set<FriendRequest> getFriendRequests() {
+	return friendRequests;
+    }
+
+    public void setFriendRequests(Set<FriendRequest> friendRequests) {
+	this.friendRequests = friendRequests;
     }
 
 }
